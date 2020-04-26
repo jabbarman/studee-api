5ccd4678fa13848cfade59593d03f97cf8e4cbba
Joseph Jabbar <jabbarman@gmail.com>
2020-04-26 14:39:51 +0100

    Create migrations, models & controllers

---
+++ app/Cclass.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Cclass extends Model
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'cclasses';
+}
---
+++ app/Http/Controllers/SegmentController.php
@@ -0,0 +1,85 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Segment;
+use Illuminate\Http\Request;
+
+class SegmentController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        //
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\segment  $segment
+     * @return \Illuminate\Http\Response
+     */
+    public function show(segment $segment)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\segment  $segment
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(segment $segment)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\segment  $segment
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, segment $segment)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\segment  $segment
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(segment $segment)
+    {
+        //
+    }
+}
